121a8d0792b4422eb843094ce88bc6c76dfb709a
Swarna Shikhar Das <swarnashikhar2002@gmail.com>
2024-05-01 00:00:26 +0530

    base url change

--- client/src/components/SignUp/index.jsx
+++ client/src/components/SignUp/index.jsx
@@ -19,7 +19,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const url = "http://localhost:8080/api/users";
+            const url = "https://playground-server-three.vercel.app/api/users";
             const { data: res } = await axios.post(url, data);
             navigate("/login");
             console.log(res.message);
